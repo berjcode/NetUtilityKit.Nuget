4ae34a90d500abee80bdedb3365c279c9e0bf265
Abdullah Balıkçı <abdullah.balikci20@ogr.atauni.edu.tr>
2023-08-24 12:13:58 +0300

    Added express parser helper class.

---
+++ Source/NetUtilityKit.LibraryNuget/NetUtilityKit.NugetPackage/ExpressionParser/ExpressionParser.cs
@@ -0,0 +1,15 @@
+﻿using System.Linq.Expressions;
+using System.Linq.Dynamic.Core;
+
+namespace NetUtilityKit.NugetPackage.ExpressionParser;
+
+public static class ExpressionParser
+{
+    public static Expression<Func<T, bool>> ParseExpression<T>(string expression)
+    {
+        var parameter = Expression.Parameter(typeof(T));
+        var lambda = DynamicExpressionParser.ParseLambda(new[] { parameter }, typeof(bool), expression);
+
+        return (Expression<Func<T, bool>>)lambda;
+    }
+}
